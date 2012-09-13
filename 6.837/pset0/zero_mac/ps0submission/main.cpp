f93913e4fabd706cc4f1251c4c8404277728c2ef
Kevin Wong <KW@Kevins-MacBook-Pro-2.local>
2012-09-12 21:01:42 -0400

    ps0 done cg

--- 6.837/pset0/zero_mac/ps0submission/main.cpp
+++ 6.837/pset0/zero_mac/ps0submission/main.cpp
@@ -28,13 +28,18 @@ vector<Vector3f> vecn;
 // This is the list of faces (indices into vecv and vecn)
 vector<vector<unsigned> > vecf;
 
-
 // You will need more global variables to implement color and position changes
 int colorIndex = 0;
 float light_x = 1.0f;
 float light_y = 1.0f;
-
-// These are convenience functions which allow us to call OpenGL 
+bool isRotating = false;
+float angle = 0.;
+bool vboLoaded = false;
+GLuint vboId;
+//GLfloat* vertices; // create vertex array
+//GLfloat* normals; // create vertex array
+float color_index = 0;
+// These are convenience functions which allow us to call OpenGL
 // methods on Vec3d objects
 inline void glVertex(const Vector3f &a) 
 { glVertex3fv(a); }
@@ -42,6 +47,14 @@ inline void glVertex(const Vector3f &a)
 inline void glNormal(const Vector3f &a) 
 { glNormal3fv(a); }
 
+// glutTimerCallback increments the angle when it's rotating:
+void timerCallback(int value){
+    if (isRotating){
+        angle++;
+        glutPostRedisplay();
+        glutTimerFunc(20, timerCallback , 0);
+    }
+}
 
 // This function is called whenever a "Normal" key press is received.
 void keyboardFunc( unsigned char key, int x, int y )
@@ -53,8 +66,21 @@ void keyboardFunc( unsigned char key, int x, int y )
         break;
     case 'c':
         // add code to change color here
+        /* //discrete color transition
         colorIndex ++;
         colorIndex = colorIndex % 4;
+        */
+        // smooth color transition:
+        color_index ++;
+        if (color_index == 101){
+            color_index = 0;
+        }
+        break;
+    case 'r':
+        isRotating = !isRotating;
+        if (isRotating){
+            glutTimerFunc(10, timerCallback , 0);
+        }
         break;
     default:
         cout << "Unhandled key press " << key << "." << endl;        
@@ -63,6 +89,7 @@ void keyboardFunc( unsigned char key, int x, int y )
 	// this will refresh the screen so that the user sees the color change
     glutPostRedisplay();
 }
+
 
 // This function is called whenever a "Special" key press is received.
 // Right now, it's handling the arrow keys.
@@ -119,8 +146,18 @@ void drawScene(void)
                                  {0.3, 0.8, 0.9, 1.0} };
     
 	// Here we use the first color entry as the diffuse color
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffColors[colorIndex]);
-
+    // glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffColors[colorIndex]);
+    float red = 1.0 - color_index/100.0;
+    float green;
+    if (color_index <= 50){
+        green = color_index/50.0;
+    } else {
+        green = 1.0 - (color_index-50)/50.0;
+    }
+    float blue = color_index/100.0;
+    GLfloat color[4] = {red, green, blue, 1.0};
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, color);
+    
 	// Define specular color and shininess
     GLfloat specColor[] = {1.0, 1.0, 1.0, 1.0};
     GLfloat shininess[] = {100.0};
@@ -143,6 +180,8 @@ void drawScene(void)
 	// it with code which draws the object you loaded.
 	//glutSolidTeapot(1.0);
     
+    glRotatef(angle, 0.0f, 1.0f, 0.0f); // Rotate our object around the y axis
+    
     for(unsigned int i=0; i < vecf.size(); i++) {
         glBegin(GL_TRIANGLES);
         glNormal3d(vecn[vecf[i][2]-1][0], vecn[vecf[i][2]-1][1], vecn[vecf[i][2]-1][2]);
@@ -153,6 +192,29 @@ void drawScene(void)
         glVertex3d(vecv[vecf[i][6]-1][0], vecv[vecf[i][6]-1][1], vecv[vecf[i][6]-1][2]);
         glEnd();
     }
+    
+    /*/ use vbo
+    glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboId);
+    
+    // enable vertex arrays
+    glEnableClientState(GL_NORMAL_ARRAY);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    
+    // before draw, specify vertex and index arrays with their offsets
+    glNormalPointer(GL_FLOAT, 0, (void*)sizeof(vertices));
+    glVertexPointer(3, GL_FLOAT, 0, 0);
+    
+    glDrawArrays(GL_TRIANGLES, 0, vecf.size()*3);
+    
+    glDisableClientState(GL_VERTEX_ARRAY);  // disable vertex arrays
+    glDisableClientState(GL_NORMAL_ARRAY);
+    
+    // it is good idea to release VBOs with ID 0 after use.
+    // Once bound with 0, all pointers in gl*Pointer() behave as real
+    // pointer, so, normal vertex array operations are re-activated
+    glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
+     */
+    
     // Dump the image to the screen.
     glutSwapBuffers();
 
@@ -224,6 +286,53 @@ void loadInput()
             // ignore other type of lines
         }
     }
+    
+    /*
+    vertices = new GLfloat[vecf.size()*9]; // create vertex array
+    normals  = new GLfloat[vecf.size()*9]; // create vertex array
+    
+    cout << vecf.size();
+    for(int i = 0; i < vecf.size(); i++) {
+        std::cout << "i" << i;
+        normals[9*i  ] = vecn[vecf[i][2]-1][0];
+        normals[9*i+1] = vecn[vecf[i][2]-1][1];
+        normals[9*i+2] = vecn[vecf[i][2]-1][2];
+        cout << "1";
+        vertices[9*i  ] = vecv[vecf[i][0]-1][0];
+        vertices[9*i+1] = vecv[vecf[i][0]-1][1];
+        vertices[9*i+2] = vecv[vecf[i][0]-1][2];
+        cout << "2";
+
+        normals[9*i+3] = vecn[vecf[i][5]-1][0];
+        normals[9*i+4] = vecn[vecf[i][5]-1][1];
+        normals[9*i+5] = vecn[vecf[i][5]-1][2];
+        cout << "3";
+
+        vertices[9*i+3] = vecv[vecf[i][3]-1][0];
+        vertices[9*i+4] = vecv[vecf[i][3]-1][1];
+        vertices[9*i+5] = vecv[vecf[i][3]-1][2];
+        cout << "4";
+
+        normals[9*i+6] = vecn[vecf[i][8]-1][0];
+        normals[9*i+7] = vecn[vecf[i][8]-1][1];
+        normals[9*i+8] = vecn[vecf[i][8]-1][2];
+        cout << "5";
+
+        vertices[9*i+6] = vecv[vecf[i][6]-1][0];
+        vertices[9*i+7] = vecv[vecf[i][6]-1][1];
+        vertices[9*i+8] = vecv[vecf[i][6]-1][2];
+    }
+    
+    glGenBuffersARB(1, &vboId);
+    glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboId);
+    glBufferDataARB(GL_ARRAY_BUFFER_ARB, sizeof(vertices)+sizeof(normals), 0, GL_STATIC_DRAW_ARB);
+    glBufferSubDataARB(GL_ARRAY_BUFFER_ARB, 0, sizeof(vertices), vertices);                             // copy vertices starting from 0 offest
+    glBufferSubDataARB(GL_ARRAY_BUFFER_ARB, sizeof(vertices), sizeof(normals), normals);                // copy normals after vertices
+    
+    delete vertices;
+    delete normals;
+    std::cout << "loading done";
+*/
 }
 
 // Main routine.
